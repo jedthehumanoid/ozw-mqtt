88f89d7766d2daad193c0b4d162541905a816cd0
se2521 <se2521@localhost.localdomain>
2016-05-19 14:31:55 +0200

    Add request and reply helpers to tools

--- tools.js
+++ tools.js
@@ -3,7 +3,6 @@ function topicMatch (topic, match) {
   match = match.split('/');
 
   for (var i = 0; i < match.length; i++) {
-    console.log(topic[i] + ' = ' + match[i]);
     if (match[i] === '+') {
     } else if (match[i] === '#') {
       return true;
@@ -14,4 +13,26 @@ function topicMatch (topic, match) {
   return true;
 }
 
+function request (request, message, callback) {
+  var id = Date.now();
+
+  this.subscribe(request + '/response/client/' + id);
+  request = request + '/request/client/' + id;
+
+  if (this.callbacks === undefined) {
+    this.callbacks = [];
+  }
+
+  this.callbacks.push(callback);
+  this.publish(request, message);
+}
+
+function onReply (topic, message) {
+  this.unsubscribe(topic);
+  var callback = this.callbacks.shift();
+  callback.apply(this, arguments);
+}
+
 exports.topicMatch = topicMatch;
+exports.request = request;
+exports.onReply = onReply;
