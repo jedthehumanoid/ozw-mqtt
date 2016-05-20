129e2e28b33f0e64e1f99d4f6b518b9cd947383f
se2521 <se2521@localhost.localdomain>
2016-05-20 09:49:44 +0200

    Changes

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
-
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://localhost');
 var topicMatch = require('./tools').topicMatch;
 client.request = require('./tools').request;
+var tools = require('./tools');
+
+var config = tools.readConfiguration('config.json');
+
+console.log(config);
 
 var onReply = require('./tools').onReply;
 
 client.on('connect', function (topic, message) {
-  client.request('server', 'pang', function (topic, message) {
+  client.request('server', 'pang', function (message) {
     console.log('callback foshissle');
   });
-  client.request('server', 'pang', function (topic, message) {
-    console.log('callback rfsadf');
+  client.request('server', 'pang', function (message) {
+    console.log(message.toString());
   });
 });
 
